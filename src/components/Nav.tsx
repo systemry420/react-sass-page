604932980ff3997e0e32c540f49f058df9787407
Kassem <systemry420@gmail.com>
2022-04-14 01:37:39 +0300

    style navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
-import '../styles/components/nav.scss'
+import '../styles/layout/nav.scss'
+import logo from '../assets/images/logo.png'
+import profile from '../assets/images/Ellipse 2.png'
 
 const Nav = () => {
   return (
     <div className='nav'>
-        Navbar
+      <img src={logo} alt='logo' />
+        <div className='nav-content'>
+          <div>
+            <form>
+              <div className='form-input'>
+                <select>
+                  <option>All Categories</option>
+
+                </select>
+
+                <input type='search' placeholder='Search' />
+
+                <button>S</button>
+              </div>
+            </form>
+          </div>
+          <div>
+            <button className='upload-btn'>Upload Video</button>
+          </div>
+          <div className='info'>
+            <img src={profile} alt='profile' />
+            <img src={profile} alt='profile' />
+          </div>
+      </div>
     </div>
   )
 }
