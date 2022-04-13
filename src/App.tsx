53e427ac37b104cf0ec795f40bb7e9826be0e68b
Kassem <systemry420@gmail.com>
2022-04-13 23:19:57 +0300

    initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import EventsSidebar from './components/EventsSidebar';
+import Main from './components/Main';
+import Nav from './components/Nav';
+import RightSidebar from './components/RightSidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Main />
+      <RightSidebar />
+      <EventsSidebar />
     </div>
   );
 }
